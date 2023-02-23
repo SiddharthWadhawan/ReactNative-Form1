31f86418684a4a1fa8731e2f1ada050b0c64d582
Siddharth Wadhawan <siddharthwadhawan7@gmail.com>
2023-02-23 15:37:56 +0530

    final

--- App.js
+++ App.js
@@ -8,25 +8,24 @@ export default function App(){
    const [age, setAge]= useState('');
    const [roll, setRoll]= useState('');
    const [dprtmnt, setdprtmnt]= useState('');
-   const [email, setEmail]= useState();
-   const [phno, setphno]= useState();
+   const [email, setEmail]= useState('');
+   const [phno, setphno]= useState('');
    
-   const inputChange1 =(val)=>{ if (val.match("^[A-Za-z_-]*$")){ setName(val);} }
-   const inputChange2 =(val)=>{ if(!isNaN(val) ) {setAge(val);} }
-   const inputChange3 =(val)=>{ if(!isNaN(val) ) {setRoll(val);} }
-   const inputChange4 =(val)=>{ setdprtmnt(val); }
-   
+   const inputChange1 =(val)=>{ if(val.match("^[A-Za-z_-]*$")) {setName(val)} }
+   const inputChange2 =(val)=>{ if(val.match("^[0-9]*$") ) {setAge(val)} }
+   const inputChange3 =(val)=>{ if(val.match("^[0-9]*$") ) {setRoll(val)} }
+   const inputChange4 =(val)=>{ setdprtmnt(val) }
+   const inputChange5 =(val)=>{ setEmail(val) }
+   const inputChange6 =(val)=>{ if(val.match("^[0-9]*$")) {setphno(val)}; }
+
    const Validations = ()=>{
-     
-        if(!name.trim() || !age.trim() || !roll.trim() || !dprtmnt.trim()){
-
-                alert('Some Field is Empty !');
-        }
+     var emailFormatCheckr = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
          
-        else{ alert(name + ' Your Form is Submitted ✅' ); }
-
-
-   }
+          if(!name.trim() || !age.trim() || !roll.trim() || !dprtmnt.trim() || !email.match(emailFormatCheckr) ){
+                alert('Fill your details properly !');
+          }
+          else{ alert(name + ' Your Form is Submitted ✅' ); }
+          }
 
    return(
         <ScrollView style={styles.scroller} bounces='tr'>
@@ -35,8 +34,8 @@ export default function App(){
           <TxtInput placehold={'Enter your age...'} uservalue={age} kbdType={'numeric'} maxlg={2} inputtextChange={inputChange2} />
           <TxtInput placehold={'Enter your Roll no...'} uservalue={roll} kbdType={'numeric'} maxlg={10} inputtextChange={inputChange3}/>
           <TxtInput placehold={'Enter your Department...'} uservalue={dprtmnt} inputtextChange={inputChange4} />
-          <TxtInput placehold={'Enter your Email...'} uservalue={email} kbdType={'email-address'}  />
-          <TxtInput placehold={'Enter your Phone no...'} uservalue={phno} kbdType={'numeric'} maxlg={10}  />
+          <TxtInput placehold={'Enter your Email...'} uservalue={email} kbdType={'email-address'} inputtextChange={inputChange5}  />
+          <TxtInput placehold={'Enter your Phone no...'} uservalue={phno} kbdType={'numeric'} maxlg={10} inputtextChange={inputChange6}  />
           <TouchableOpacity onPress={()=>Validations()} >
             <Text style={styles.substyle}>SUBMIT</Text>
         </TouchableOpacity>
